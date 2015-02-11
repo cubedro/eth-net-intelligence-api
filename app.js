b69b6a31b3ff34c38b8e0a4dfb704ac79ad91b91
Marian Oancea <contact@siteshop.ro>
2015-02-12 00:54:33 +0200

    added config and node model

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 var app = express();
 app.http().io();
 
-//
+var config = require('./config');
+
+var node = new require('./lib/node')(config);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+	name: 'Node',
+	type: 'C++',
+	os: 'linux',
+	rpcHost: 'localhost',
+	rpcPort: '8080'
+};
+
+module.exports = config;
