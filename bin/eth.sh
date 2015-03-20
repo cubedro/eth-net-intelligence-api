e70f08914f0b2fd817a66f84aa0f857178892f27
cubedro <contact@siteshop.ro>
2015-03-20 08:17:13 +0200

    updated to ethereum.js 0.1.2 and refactoring

--- bin/eth.sh
+++ bin/eth.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 trap "exit" INT
-/home/ubuntu/bin/eth -b -x 15 -l 30303 -r poc-8.ethdev.com -p 30303 -m off -j
+if [ -f /usr/bin/ethereum ]
+then
+	ethereum -rpc true -rpcport 8080
+else
+	eth -x 15 -l 30303 -r poc-8.ethdev.com -p 30303 -m off -j
+fi
