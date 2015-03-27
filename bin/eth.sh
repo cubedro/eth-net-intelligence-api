9f62909dca410628a91311830d0590df7fa89f92
cubedro <contact@siteshop.ro>
2015-03-27 11:21:17 +0200

    added verbosity flag

--- bin/eth.sh
+++ bin/eth.sh
@@ -4,5 +4,5 @@ if [ -f /usr/bin/ethereum ]
 then
 	ethereum -rpc true
 else
-	eth -x 15 -l 30303 -r poc-8.ethdev.com -p 30303 -m off -j
+	eth -x 15 -l 30303 -r poc-8.ethdev.com -p 30303 -m off -v 1 -j
 fi
