24c7a73b62c13b89377cd5b5bb02e7f2a5208ee9
cubedro <contact@siteshop.ro>
2015-04-03 02:29:09 +0300

    updated maxpeers to 50

--- bin/eth.sh
+++ bin/eth.sh
@@ -2,7 +2,7 @@
 trap "exit" INT
 if [[ -f /usr/bin/geth ]];
 then
-	geth --rpc true --loglevel 1
+	geth --rpc true --maxpeers 50 --loglevel 1
 else
-	eth -x 15 -l 30303 -n off -m off -v 1 -j
+	eth -x 50 -l 30303 -n off -m off -v 1 -j
 fi
