f36db1879bee7446e03ba06788073aac20495145
cubedro <contact@siteshop.ro>
2015-04-03 02:46:02 +0300

    fixed geth params

--- bin/eth.sh
+++ bin/eth.sh
@@ -2,7 +2,7 @@
 trap "exit" INT
 if [[ -f /usr/bin/geth ]];
 then
-	geth --rpc true --maxpeers "50" --loglevel "1"
+	geth -rpc -maxpeers "50" -loglevel "1"
 else
 	eth -x 50 -l 30303 -n off -m off -v 1 -j
 fi
