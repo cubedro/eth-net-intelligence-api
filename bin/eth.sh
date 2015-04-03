751b13db049f7374a2ce7c77c15518d695619c7b
cubedro <contact@siteshop.ro>
2015-04-03 19:39:25 +0300

    added poc-9 to eth as bootstrap node

--- bin/eth.sh
+++ bin/eth.sh
@@ -4,5 +4,5 @@ if [[ -f /usr/bin/geth ]];
 then
 	geth -rpc -rpcport "8080" -maxpeers "50" -loglevel "1"
 else
-	eth -b -x 50 -r 52.16.188.185 -p 30303 -m off -n off -v 0 -j
+	eth -b -x 50 -r poc-9.ethdev.com -p 30303 -m off -n off -v 0 -j
 fi
